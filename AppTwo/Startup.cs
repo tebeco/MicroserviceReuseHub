7bcbfad559186f19286470ede9bbd752f21f4b1d
TeBeCo <tebeco@gmail.com>
2020-08-06 23:32:21 +0200

    Adding Azure SignalR

--- AppTwo/Startup.cs
+++ AppTwo/Startup.cs
@@ -20,7 +20,7 @@ namespace AppTwo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR();
+            services.AddSignalR().AddAzureSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
