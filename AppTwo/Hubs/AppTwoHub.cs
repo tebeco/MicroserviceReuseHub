9cb9b051520fd121aa1724a5185cd49d4fa7791e
TeBeCo <tebeco@gmail.com>
2020-08-06 21:10:27 +0200

    Bump AppTwo

---
+++ AppTwo/Hubs/AppTwoHub.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace AppTwo.Hubs
+{
+    public class AppTwoHub : Hub
+    {
+        private readonly ILogger<AppTwoHub> _logger;
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+
+        public AppTwoHub(ILogger<AppTwoHub> logger, IHostApplicationLifetime hostApplicationLifetime)
+        {
+            _logger = logger;
+            _hostApplicationLifetime = hostApplicationLifetime;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            _logger.LogInformation("+++++++++++++ CLIENT JOIN");
+            return Task.CompletedTask;
+        }
+
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            _logger.LogInformation("+++++++++++++ CLIENT LEFT");
+            return Task.CompletedTask;
+        }
+
+        public async Task DoBar()
+        {
+            _logger.LogInformation("Doing 'Bar'");
+            await Clients.All.SendAsync("Bar");
+        }
+    }
+}
--- AppTwo/Startup.cs
+++ AppTwo/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AppTwo.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Shared;
 
 namespace AppTwo
 {
@@ -25,7 +20,7 @@ namespace AppTwo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +39,7 @@ namespace AppTwo
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapHub<AppTwoHub>($"/{nameof(AppTwoHub)}");
             });
         }
     }
