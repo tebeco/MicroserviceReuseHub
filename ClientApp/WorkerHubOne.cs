873ba49cb6e36c76be0899daccc545c1c74c63a2
TeBeCo <TeBeCo@gmail.com>
2020-08-07 12:03:05 +0200

    Remove Foo / Bar code also AsyncStream helpers

--- ClientApp/WorkerHubOne.cs
+++ ClientApp/WorkerHubOne.cs
@@ -25,40 +25,12 @@ namespace ClientApp
 
             await _appOneClient.OnConnectedAsync();
 
-            _appOneClient.HubConnection.On("Foo", () =>
-            {
-                _logger.LogInformation("On Foo");
-            });
-
-            _appOneClient.HubConnection.On("DoingFoo", () =>
-            {
-                _logger.LogInformation("The hub is doing Foo");
-            });
-
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Calling DoFoo");
-                var receivedStream = _appOneClient.DoFooAsync(GenerateDataStream());
-                await EnumerateStream(receivedStream);
                 await Task.Delay(4000);
             }
         }
 
-        public async IAsyncEnumerable<int> GenerateDataStream()
-        {
-            for (var i = 0; i < 5; i++)
-            {
-                yield return i;
-                await Task.Delay(500);
-            }
-            //After the for loop has completed and the local function exits the stream completion will be sent.
-        }
-
-        private async Task EnumerateStream(IAsyncEnumerable<int> receivedStream)
-        {
-            await foreach (var i in receivedStream)
-            {
-                _logger.LogInformation("Received back: {item}", i);
             }
         }
     }
